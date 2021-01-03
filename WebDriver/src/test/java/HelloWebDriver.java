dabd5ab34cf8883fd337f4c1a8ce6092bb785e79
Kudelich Dmitry <dima.kudelich2@mail.ru>
2021-01-03 19:26:01 +0300

    working with wait's

--- WebDriver/src/test/java/HelloWebDriver.java
+++ WebDriver/src/test/java/HelloWebDriver.java
@@ -2,16 +2,34 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
 
 public class HelloWebDriver {
     public static void main(String[] args) throws InterruptedException {
         WebDriver driver = new ChromeDriver();
         driver.get("https://by.wildberries.ru/");
-        WebElement searchInput = driver.findElement(By.id("tbSrch"));
+
+        new WebDriverWait(driver,10)
+                .until(ExpectedConditions.presenceOfElementLocated(By.id("tbSrch")));
+
+        //WebElement searchInput = driver.findElement(By.id("tbSrch"));
+        WebElement searchInput = waitForElementLocatedBy(driver,By.id("tbSrch"));
         searchInput.sendKeys("book");
-        WebElement searchBtn = driver.findElement(By.id("btnSrch"));
-        searchBtn.click();
-        Thread.sleep(2000);
+
+        List<WebElement> searchBtn = driver.findElements(By.id("btnSrch"));
+        searchBtn.get(0).click();
+
+        new WebDriverWait(driver,10)
+                .until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.id("btnSrch")));
+
         driver.quit();
+    }
+
+    private static WebElement waitForElementLocatedBy(WebDriver driver,By by) {
+        return new WebDriverWait(driver,10)
+                    .until(ExpectedConditions.presenceOfElementLocated(by));
     }
 }
